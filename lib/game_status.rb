b13f349814958e62f854a50fb7f951ccd20c58b6
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2020-11-13 01:43:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,6 +54,16 @@ WIN_COMBINATIONS= [
     end
   end
   
-  def winner
-    
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
+end
